cbc4495b2bc72686ef5a3da630470be492894f86
Riju <aryamansen26@gmail.com>
2025-01-19 01:05:27 +0530

    update

--- sudhaar/src/pages/aboutus.jsx
+++ sudhaar/src/pages/aboutus.jsx
@@ -6,6 +6,19 @@ const Aboutus = () => {
   return(
     <>
       <Navbar/>
+
+      <div className={styles.container}>
+        {/* Main Box */}
+        <div className={styles.missionBox}>
+          <h1 className={styles.heading}>What We Do?</h1>
+          <p className={styles.missionText}>
+            To help individuals figure out the outcome
+            of their loan approvals and additionally better their outcome by providing in depth analysis of the
+            information provided by them.
+          </p>
+          <button className={styles.readMoreButton}>READ MORE</button>
+        </div>
+      </div>
     </>
   );
 };

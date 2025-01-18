1283457c7a3181b0564d2f656f9ca4e690950073
pratyush0000 <121302647+pratyush0000@users.noreply.github.com>
2025-01-18 18:29:12 +0530

    update

--- sudhaar/src/pages/aboutus.jsx
+++ sudhaar/src/pages/aboutus.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Aboutus.module.css';
+import Navbar from "../components/Navbar";
 
 const Aboutus = () => {
   return(
     <>
-    helo from aboutus
+      <Navbar/>
     </>
   );
 };
--- sudhaar/src/pages/predict.jsx
+++ sudhaar/src/pages/predict.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from "../components/Navbar";
 
 const predict = () => {
   return (
     <>
-      Hello from predict
+      <Navbar/>
     </>
   );
 };
